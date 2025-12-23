738a6f789168009c83ec87db9eb41b29f14925f1
zjsan <zjsantos25@gmail.com>
2025-12-23 13:59:21 +0800

    added contents

---
+++ laravel/resources/js/Pages/public/LandingPage.vue
@@ -0,0 +1,6 @@
+<template>
+    <LandingPageLayout title="Ride Smarter Save Together" message="testing" />
+</template>
+<script>
+import LandingPageLayout from "../layout/LandingPageLayout.vue";
+</script>
