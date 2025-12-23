0d44c44d7c5635ccd82c70b4cc64d1f73e9eb6c7
zjsan <zjsantos25@gmail.com>
2025-12-23 13:54:08 +0800

    created layout template for landing page

---
+++ laravel/resources/js/Pages/layout/LandingPageLayout.vue
@@ -0,0 +1,14 @@
+<template>
+    <div class="container mx-auto">
+        <h1 class="text-2xl font-bold" :class="titleClass">{{ title }}</h1>
+        <h2 class="text-xl font-bold"></h2>
+        <p class="text-gray-600 leading-relaxed">{{ message }}</p>
+    </div>
+</template>
+<script>
+const props = defineProps({
+    title: { type: String, required: true },
+    message: { type: String, required: true },
+    titleClass: { type: String, default: "text-white" }, // allows color override
+});
+</script>
