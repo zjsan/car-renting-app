23fdf52278aff7ee49965cb892236a57f0d4f54f
zjsan <zjsantos25@gmail.com>
2025-12-23 14:55:08 +0800

    added router instance

--- laravel/resources/js/app.js
+++ laravel/resources/js/app.js
@@ -1,8 +1,10 @@
-import './bootstrap';
-import { createApp } from 'vue';
-import { createPinia } from 'pinia';
-import App from './Pages/App.vue';
+import "./bootstrap";
+import { createApp } from "vue";
+import { createPinia } from "pinia";
+import App from "./Pages/App.vue";
+import { router } from "./router";
 
 const app = createApp(App);
 app.use(createPinia());
-app.mount('#app');
+app.use(router);
+app.mount("#app");
