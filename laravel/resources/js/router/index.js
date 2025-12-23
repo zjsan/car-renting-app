f1cb0d9ab4bc6bafe60f2aa63a43153a1547172f
zjsan <zjsantos25@gmail.com>
2025-12-23 14:39:08 +0800

    updated router

--- laravel/resources/js/router/index.js
+++ laravel/resources/js/router/index.js
@@ -2,10 +2,7 @@ import { createMemoryHistory, createRouter } from "vue-router";
 
 import LandingPage from "../Pages/public/LandingPage.vue";
 
-const routes = [
-    { path: "/", component: LandingPage },
-    { path: "/about", component: AboutView },
-];
+const routes = [{ path: "/", component: LandingPage }];
 
 export const router = createRouter({
     history: createMemoryHistory(),
