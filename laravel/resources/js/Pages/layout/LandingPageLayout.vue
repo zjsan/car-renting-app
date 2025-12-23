57e630ab6e89913e3fd66347abe6ffd1f2e97f43
zjsan <zjsantos25@gmail.com>
2025-12-23 13:55:59 +0800

    added new props

--- laravel/resources/js/Pages/layout/LandingPageLayout.vue
+++ laravel/resources/js/Pages/layout/LandingPageLayout.vue
@@ -1,13 +1,14 @@
 <template>
     <div class="container mx-auto">
         <h1 class="text-2xl font-bold" :class="titleClass">{{ title }}</h1>
-        <h2 class="text-xl font-bold"></h2>
+        <h2 class="text-xl font-bold" :class="titleClass">{{ header2 }}</h2>
         <p class="text-gray-600 leading-relaxed">{{ message }}</p>
     </div>
 </template>
 <script>
 const props = defineProps({
     title: { type: String, required: true },
+    header2: { type: String, required: true },
     message: { type: String, required: true },
     titleClass: { type: String, default: "text-white" }, // allows color override
 });
