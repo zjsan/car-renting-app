2847f46aba918e24bf86e8102c82fe1557de3af2
zjsan <zjsantos25@gmail.com>
2026-01-23 10:31:09 +0800

    added buttons in hero section

--- laravel/resources/js/Pages/components/Basebutton.vue
+++ laravel/resources/js/Pages/components/Basebutton.vue
@@ -3,7 +3,7 @@ import { computed } from "vue";
 
 interface Props {
     variant?: "primary" | "secondary" | "outline" | "danger" | "ghost";
-    size?: "sm" | "md" | "lg";
+    size?: "sm" | "md" | "lg" | "xl";
     type?: "button" | "submit" | "reset";
     disabled?: boolean;
 }
@@ -24,7 +24,7 @@ const baseStyles =
 const variants = {
     primary: "bg-blue-600 text-white hover:bg-blue-700 focus:ring-blue-500",
     secondary:
-        "bg-slate-200 text-slate-900 hover:bg-slate-300 focus:ring-slate-400",
+        "bg-slate-100 text-slate-900 hover:bg-slate-200 focus:ring-slate-400",
     outline:
         "border border-slate-300 bg-transparent hover:bg-slate-100 text-slate-700",
     danger: "bg-red-600 text-white hover:bg-red-700 focus:ring-red-500",
@@ -36,6 +36,7 @@ const sizes = {
     sm: "px-3 py-1.5 text-xs",
     md: "px-4 py-2 text-sm",
     lg: "px-6 py-3 text-base",
+    xl: "px-8 py-4 text-lg",
 };
 
 // Combine classes dynamically
--- laravel/resources/js/Pages/public/HomeView.vue
+++ laravel/resources/js/Pages/public/HomeView.vue
@@ -6,13 +6,12 @@
         bgImage="images/4de153d9-a527-4f2e-b175-6942a70b3607.png"
         sectionClass="min-h-screen"
     >
-        <BaseButton variant="primary">Save Changes</BaseButton>
-        <p>
-            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Iusto,
-            accusantium. Veritatis, reprehenderit non. Culpa tempore ad officia
-            blanditiis? Molestias inventore aliquid, unde velit vel reiciendis
-            dolor recusandae numquam. Veritatis, excepturi?
-        </p>
+        <div class="mt-5 flex flex-row items-center space-x-4 justify-center">
+            <BaseButton variant="secondary" size="xl">Rent a Car</BaseButton>
+            <BaseButton variant="primary" size="xl"
+                >Discover our fleet</BaseButton
+            >
+        </div>
     </LandingPageLayout>
 
     <!-- About Us Section -->
@@ -26,6 +25,8 @@
         </p>
     </LandingPageLayout>
 </template>
+
 <script setup>
 import LandingPageLayout from "../layout/LandingPageLayout.vue";
+import BaseButton from "../components/Basebutton.vue";
 </script>
