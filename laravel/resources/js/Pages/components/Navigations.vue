2b5f04e295e24b97d6109836ed520a2bebe592e0
zjsan <zjsantos25@gmail.com>
2026-02-28 17:14:53 +0800

    working with mobile nav layout

--- laravel/resources/js/Pages/components/Navigations.vue
+++ laravel/resources/js/Pages/components/Navigations.vue
@@ -3,7 +3,23 @@
 
         <!--Mobile-->
         <div>
+            <div>
+                <nav>
+                    <!--burger icon-->
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
+                    <path stroke-linecap="round" stroke-linejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
+                    </svg>
 
+                    <!--close (x) icon -->
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
+                    <path stroke-linecap="round" stroke-linejoin="round" d="M6 18 18 6M6 6l12 12" />
+                    </svg>
+
+                </nav>
+            </div>
+            <nav>
+
+            </nav>
         </div>
     </div>
 </template>
