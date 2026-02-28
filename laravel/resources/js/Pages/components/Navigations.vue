9afbc538953313f0f90d8890592f3396a6399789
zjsan <zjsantos25@gmail.com>
2026-02-28 17:24:51 +0800

    added event listener for button click

--- laravel/resources/js/Pages/components/Navigations.vue
+++ laravel/resources/js/Pages/components/Navigations.vue
@@ -4,8 +4,8 @@
         <!--Mobile-->
         <div>
             <div>
-                <nav>
-                    <!--burger icon-->
+                <button @click="toggleMenu">
+                     <!--burger icon-->
                     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
                     <path stroke-linecap="round" stroke-linejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
                     </svg>
@@ -14,8 +14,7 @@
                     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
                     <path stroke-linecap="round" stroke-linejoin="round" d="M6 18 18 6M6 6l12 12" />
                     </svg>
-
-                </nav>
+                </button>
             </div>
             <nav>
 
