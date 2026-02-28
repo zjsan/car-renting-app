56679339e7f5bc67c2c490b9c724cd3ef19ef487
zjsan <zjsantos25@gmail.com>
2026-02-28 16:20:26 +0800

    created object for cars in featured vehicles section

---
+++ laravel/resources/js/Pages/public/cars.js
@@ -0,0 +1,20 @@
+const cars = 
+[
+    {
+        name: "Toyota Vios 2020",
+        description: "Comfortable sedan, perfect for city drives and long trips",
+        image: ""
+    },
+    {
+        name: "Montero Sport 2016",
+        description: "A popular 7-seater mid-size SUV.",
+        image: ""
+    },
+    {
+        name: "Toyota Hiace 2018",
+        description: "A versatile, spacious, and reliable solution for large group travel.",
+        image: ""
+    }
+]
+
+export default cars;
