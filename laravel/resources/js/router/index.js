5cad25c1cdbc3a8aa28955667085ae940f67d6e0
zjsan <zjsantos25@gmail.com>
2026-03-04 15:43:36 +0800

    inserted login component in vue router

--- laravel/resources/js/router/index.js
+++ laravel/resources/js/router/index.js
@@ -1,8 +1,12 @@
 import { createMemoryHistory, createRouter } from "vue-router";
 
 import LandingPage from "../Pages/public/LandingPage.vue";
+import Login from "../Pages/public/Login.vue";
 
-const routes = [{ path: "/", component: LandingPage }];
+const routes = [
+    { path: "/", component: LandingPage },
+    { path: "/login", component: Login },
+];
 
 export const router = createRouter({
     history: createMemoryHistory(),
