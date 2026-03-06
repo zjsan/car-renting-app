73ac12e0454be093b84a9e1c0a0d7dc614c2169c
zjsan <zjsantos25@gmail.com>
2026-03-06 15:02:05 +0800

    created login form

--- laravel/resources/js/Pages/public/Login.vue
+++ laravel/resources/js/Pages/public/Login.vue
@@ -6,35 +6,52 @@
         >
         <div class="mt-2 max-w-2xl">
             <h1 class="text-4xl font-bold text-white mb-6">
-                Rent Your Dream Car Today!
+                Start your Ilocos Adventure Today!
             </h1>
             <p class="text-lg text-gray-300 mb-8">
                 Experience the freedom of the open road with our wide selection
-                of vehicles. Whether you're looking for a sleek sports car or a
+                of vehicles. Whether you're looking for a sleek sedans or a
                 spacious SUV, we have the perfect ride for you.
             </p>
             <div
-                class="flex flex-col space-y-4 bg-white p-6 rounded-lg shadow-md"
+                class="flex flex-col justify-center min-h-full space-y-4 bg-white py-15 px-20 rounded-lg shadow-md"
             >
                 <form action="">
                     <input
                         type="text"
                         id="email"
-                        placeholder="Email/Username"
-                        class="px-4 py-2 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500 bg-white"
+                        placeholder="Email"
+                        class="px-4 py-2 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500 bg-white border border-gray-700"
                     />
                     <input
                         type="password"
                         id="password"
                         placeholder="Password"
-                        class="px-4 py-2 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500 bg-white mt-4"
+                        class="px-4 py-2 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500 bg-white mt-4 border border-gray-700"
                     />
                     <button
                         type="submit"
-                        class="px-4 py-2 bg-blue-500 text-white rounded-md hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-500"
+                        class="mt-8 px-22 py-2 bg-blue-500 text-white font-bold rounded-md hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-500"
                     >
                         Login
                     </button>
+
+                    <p class="text-sm text-gray-500 mt-4">
+                        Forgot Password?
+                        <a
+                            href="/register"
+                            class="text-blue-500 hover:underline"
+                        >
+                            Click here to reset
+                        </a>
+                    </p>
+
+                    <p class="text-sm text-gray-500 mt-4">
+                        Don't have an account?
+                        <a href="#" class="text-blue-500 hover:underline">
+                            Sign up
+                        </a>
+                    </p>
                 </form>
             </div>
         </div>
