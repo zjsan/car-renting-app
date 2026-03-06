d133ec1ed2a07e04c43f2701696256a387a22539
zjsan <zjsantos25@gmail.com>
2026-03-06 11:43:52 +0800

    updated catch all route

--- laravel/routes/web.php
+++ laravel/routes/web.php
@@ -3,7 +3,7 @@
 use Illuminate\Support\Facades\Route;
 
 //catch-all route that returns a single Blade view
-Route::get('/{any}', function () {
+Route::get('/{any?}', function () {
     return view('welcome'); 
 })->where('any', '.*');
 
