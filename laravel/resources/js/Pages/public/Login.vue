d689066cb636cffb93b5ebb8da6e50829d3b7018
zjsan <zjsantos25@gmail.com>
2026-03-06 14:41:04 +0800

    creating the login form

--- laravel/resources/js/Pages/public/Login.vue
+++ laravel/resources/js/Pages/public/Login.vue
@@ -13,9 +13,29 @@
                 of vehicles. Whether you're looking for a sleek sports car or a
                 spacious SUV, we have the perfect ride for you.
             </p>
-            <div class="flex space-x-4">
-                <Button variant="primary" size="lg">Get Started</Button>
-                <Button variant="outline" size="lg">Learn More</Button>
+            <div
+                class="flex flex-col space-y-4 bg-white p-6 rounded-lg shadow-md"
+            >
+                <form action="">
+                    <input
+                        type="text"
+                        id="email"
+                        placeholder="Email/Username"
+                        class="px-4 py-2 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500 bg-white"
+                    />
+                    <input
+                        type="password"
+                        id="password"
+                        placeholder="Password"
+                        class="px-4 py-2 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500 bg-white mt-4"
+                    />
+                    <button
+                        type="submit"
+                        class="px-4 py-2 bg-blue-500 text-white rounded-md hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-500"
+                    >
+                        Login
+                    </button>
+                </form>
             </div>
         </div>
     </section>
