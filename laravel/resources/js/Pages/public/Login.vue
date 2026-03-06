f678020e42359d6b565bb632e4206a9daf52e5cd
zjsan <zjsantos25@gmail.com>
2026-03-06 15:23:57 +0800

    updated login page

--- laravel/resources/js/Pages/public/Login.vue
+++ laravel/resources/js/Pages/public/Login.vue
@@ -6,12 +6,11 @@
         >
         <div class="mt-2 max-w-2xl">
             <h1 class="text-4xl font-bold text-white mb-6">
-                Start your Ilocos Adventure Today!
+                Ready for Your Next Ride?
             </h1>
             <p class="text-lg text-gray-300 mb-8">
-                Experience the freedom of the open road with our wide selection
-                of vehicles. Whether you're looking for a sleek sedans or a
-                spacious SUV, we have the perfect ride for you.
+                From the sand dunes to the heritage sites, your perfect ride is
+                waiting. Sign in to continue your story.
             </p>
             <div
                 class="flex flex-col justify-center min-h-full space-y-4 bg-white py-15 px-20 rounded-lg shadow-md"
@@ -31,9 +30,9 @@
                     />
                     <button
                         type="submit"
-                        class="mt-8 px-22 py-2 bg-blue-500 text-white font-bold rounded-md hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-500"
+                        class="mt-8 px-20 py-2 bg-blue-500 text-white font-bold rounded-md hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-500"
                     >
-                        Login
+                        Sign in
                     </button>
 
                     <p class="text-sm text-gray-500 mt-4">
