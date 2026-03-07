ad6ece0ad15c64cacdaf767b30ba8eb32ca76e1c
zjsan <zjsantos25@gmail.com>
2026-03-07 18:46:20 +0800

    updating setter and actions

--- laravel/resources/js/stores/auth.js
+++ laravel/resources/js/stores/auth.js
@@ -1,11 +1,28 @@
 import { defineStore } from 'pinia'
+import api from "./api/axios.js"
+import {ref} from 'vue';
 
 // Example structure
 export const useAuthStore = defineStore('auth', {
-  state: () => ({ user: JSON.parse(localStorage.getItem('user')) || null }),
+  state: () => ({
+     user: JSON.parse(localStorage.getItem('user')) || null,
+     loading: false, //track asynch reques
+     error: null, //track errors
+    
+    }),
   getters: { isAuthenticated: (state) => !!state.user },
   actions: {
-    async login(credentials) { /* API call & set user */ },
+    async login(credentials) {
+         /* API call & set user */
+
+         try {
+            loading = true
+
+            
+         } catch (error) {
+            
+         }
+     },
     logout() { /* Clear user & storage */ },
   },
 });
