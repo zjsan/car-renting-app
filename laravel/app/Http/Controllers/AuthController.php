fc6d3f41c3c0f0d341d2554f49d5568391c6388a
zjsan <zjsantos25@gmail.com>
2026-03-07 19:20:54 +0800

    added block for fetching and verifying credentials

--- laravel/app/Http/Controllers/AuthController.php
+++ laravel/app/Http/Controllers/AuthController.php
@@ -5,6 +5,8 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use Illuminate\Http\RedirectResponse;
 use Illuminate\Support\Facades\Auth;
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
 
 class AuthController extends Controller
 {
@@ -15,5 +17,24 @@ class AuthController extends Controller
             'email' => ['required','email'],
             'password' => ['required']
         ]);
+
+        $user = User::where('email', $request->email)->first(); //fetch user
+
+        //verify user credentials
+        if (!$user || !Hash::check($request->password, $user->password)) {
+            return response()->json([
+                'message' => 'Invalid credentials'
+            ], 401);
+        }
+
+        $token = $request->user()->createToken($request->token_name);//create token
+        
+
+        if (Auth::attempt($credentials)) {
+            
+ 
+            return redirect()->intended('dashboard');
+        }
+ 
     }
 }
