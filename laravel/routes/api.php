d7bf1d1cd564e898ed78e8ed0bc54812834c2819
zjsan <zjsantos25@gmail.com>
2026-03-07 18:15:02 +0800

    updating contents

--- laravel/routes/api.php
+++ laravel/routes/api.php
@@ -2,6 +2,10 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
+
+
+Route::post('login', [AuthController::class, 'login']);
+Route::post('logout', [AuthController::class, 'logout'])->middleware('auth:sanctum');
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
