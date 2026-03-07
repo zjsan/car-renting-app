cab61d3b99df7de3f73dc0ee159e4b3cabff20b4
zjsan <zjsantos25@gmail.com>
2026-03-07 16:53:24 +0800

    invoke statefulApi in middleware method and installed sanctum

--- laravel/bootstrap/app.php
+++ laravel/bootstrap/app.php
@@ -12,7 +12,9 @@ return Application::configure(basePath: dirname(__DIR__))
     )
     ->withMiddleware(function (Middleware $middleware): void {
         //
+         $middleware->statefulApi();
     })
     ->withExceptions(function (Exceptions $exceptions): void {
         //
     })->create();
+    
