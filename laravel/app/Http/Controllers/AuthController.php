ddf8217adfad5bbfb61a1f04216d22ecc699b238
zjsan <zjsantos25@gmail.com>
2026-03-07 18:58:35 +0800

    creating login function

--- laravel/app/Http/Controllers/AuthController.php
+++ laravel/app/Http/Controllers/AuthController.php
@@ -3,8 +3,17 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Http\RedirectResponse;
+use Illuminate\Support\Facades\Auth;
 
 class AuthController extends Controller
 {
     //
+    public function login(Request $request){
+
+        $credentials = $request->validate([
+            'email' => ['required','email'],
+            'password' => ['required']
+        ]);
+    }
 }
