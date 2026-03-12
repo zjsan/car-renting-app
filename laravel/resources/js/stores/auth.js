d5ad63d8962937b08423fee48e1600bcda426af2
zjsan <zjsantos25@gmail.com>
2026-03-12 16:14:32 +0800

    updating login block

--- laravel/resources/js/stores/auth.js
+++ laravel/resources/js/stores/auth.js
@@ -1,29 +1,36 @@
-import { defineStore } from 'pinia'
-import api from "./api/axios.js"
-import {ref} from 'vue';
+import { defineStore } from "pinia";
+import api from "./api/axios.js";
+import { ref } from "vue";
 
 // Example structure
-export const useAuthStore = defineStore('auth', {
-  state: () => ({
-     user: JSON.parse(localStorage.getItem('user')) || null,
-     loading: false, //track asynch reques
-     error: null, //track errors
-    
+export const useAuthStore = defineStore("auth", {
+    state: () => ({
+        user: JSON.parse(localStorage.getItem("user")) || null,
+        loading: false, //track asynch reques
+        error: null, //track errors
+        isAuthenticated: false, //track auth status
     }),
-  getters: { isAuthenticated: (state) => !!state.user },
-  actions: {
-    async login(credentials) {
-         /* API call & set user */
+    getters: { isAuthenticated: (state) => !!state.user },
+    actions: {
+        async login(credentials) {
+            /* API call & set user */
 
-         try {
-            loading = true
+            try {
+                loading = true;
 
-            api.get('/sanctum/csrf-cookie');//intialize csrf protection
-            
-         } catch (error) {
-            
-         }
-     },
-    logout() { /* Clear user & storage */ },
-  },
+                api.get("/sanctum/csrf-cookie"); //intialize csrf protection
+                const response = await api.post("/login", credentials);
+                user.value = response.data.user;
+                localStorage.setItem("user", JSON.stringify(user.value));
+                isAuthenticated.value = true;
+            } catch (error) {
+                error.value = error;
+            } finally {
+                loading.value = false;
+            }
+        },
+        logout() {
+            /* Clear user & storage */
+        },
+    },
 });
